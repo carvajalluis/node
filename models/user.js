c9085e0fe61730c9f64d2bcc1d0e142b41759157
Luis Carvajal <luisalbcarvajal@gmail.com>
2020-01-02 11:14:05 -0600

    feat(password reset): include new password reset functionality

--- models/user.js
+++ models/user.js
@@ -7,6 +7,8 @@ const userSchema = new Schema({
   userName: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  resetToken: String,
+  resetExpiration: Date,
   cart: {
     items: [
       {
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,55 @@
 const express = require("express");
+const { check, body } = require("express-validator");
 const authCtrl = require("../controllers/auth");
 const isAuth = require("../middleware/is-auth");
+const User = require("../models/user");
 const router = express.Router();
 
 router.get("/login", authCtrl.getLogin);
-router.post("/login", authCtrl.postLogin);
-router.post("/logout", isAuth, authCtrl.postLogout);
+router.get("/reset/:token", authCtrl.getNewPassword);
+router.get("/reset", authCtrl.getReset);
 router.get("/signup", authCtrl.getSignup);
-router.post("/signup", authCtrl.postSignup);
+router.post(
+  "/login",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+  ],
+  authCtrl.postLogin
+);
+router.post("/logout", isAuth, authCtrl.postLogout);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        return User.findOne({
+          $or: [{ email: value }, { userName: value.split("@").shift() }]
+        }).then(user => {
+          if (user) {
+            return Promise.reject(
+              "Email already exists, please pick a different email"
+            );
+          }
+        });
+      }),
+    body("password")
+      .isLength({ min: 6, max: 10 })
+      .withMessage("please use at least 6 characters"),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match");
+      }
+      return true;
+    })
+  ],
+
+  authCtrl.postSignup
+);
+router.post("/reset", authCtrl.postReset);
+router.post("/new-password", authCtrl.postNewPassword);
 
 module.exports = router;
